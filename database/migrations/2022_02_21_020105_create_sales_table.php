d833a0b1743a0e7e24dcb1f5b81087c2335a4bce
yoshiki <katoyoshi9814@gmail.com>
2022-02-25 18:33:57 +0900

    マイグレーションファイルの作成とlaravelの日本語化

--- database/migrations/2022_02_21_020105_create_sales_table.php
+++ database/migrations/2022_02_21_020105_create_sales_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateSalesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,9 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('sales', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('user_name', 255);
-            $table->string('email', 255)->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('product_id')->primary(false);
             $table->timestamps();
         });
     }
@@ -30,6 +27,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('sales');
     }
 }
