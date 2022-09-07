8769de4b58f5c89b6919b3d58427fa05ab29acf9
yoshiki <katoyoshi9814@gmail.com>
2022-09-07 14:21:37 +0900

    登録画像の表示と縦表示に変更

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -3,24 +3,15 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-12 col-md-offset-2">
+        <div class="col-md-12 d-flex flex-column col-md-offset-2">
             <span>商品番号:{{ $product->id }}</span>
-            <span>商品画像:<img src="{{ $product->img_path }}" width="150" height="150"></span>
+            <span>商品画像:<img src="{{ asset('/storage/' .$product->img_path) }}" width="150" height="150"></span>
             <span>商品名:{{ $product->name }}</span>
             <span>値段:{{ $product->price }}</span>
             <span>在庫:{{ $product->stock }}</span>
-            <span>メーカー:{{ $product->maker }}</span>
+            <span>メーカー:{{ $company->company_name }}</span>
         </div>
     </div>
 </div>
 
-<script>
-    function checkDelete() {
-        if (window.confirm('削除してよろしいですか？')) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-</script>
 @endsection
