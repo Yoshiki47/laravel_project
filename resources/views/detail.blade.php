13d798577d2a8daf7fbcda8606a383adf82656ac
yoshiki <katoyoshi9814@gmail.com>
2022-09-27 12:10:26 +0900

    詳細画面で画像表示

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -5,7 +5,13 @@
     <div class="row">
         <div class="col-md-12 d-flex flex-column col-md-offset-2">
             <span>商品番号:{{ $product->id }}</span>
-            <span>商品画像:<img src="{{ asset('/storage/' .$product->img_path) }}" width="150" height="150"></span>
+            <span>商品画像:
+                @if ($product->img_path === null)
+                <img src="{{ asset('/storage/noimage.png') }}" alt="noimage" width="150" height="150">
+                @else
+                <img src="{{ asset('/storage/' .$product->img_path) }}" width="150" height="150">
+                @endif
+            </span>
             <span>商品名:{{ $product->name }}</span>
             <span>値段:{{ $product->price }}</span>
             <span>在庫:{{ $product->stock }}</span>
